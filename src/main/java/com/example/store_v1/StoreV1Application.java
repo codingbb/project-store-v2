453330c82dcd44063bad492a41d97ee79b309de8
SCHcoding <codingb007@gmail.com>
2024-04-24 17:24:35 +0900

    mySQL 환경 세팅 완료

---
+++ src/main/java/com/example/store_v1/StoreV1Application.java
@@ -0,0 +1,13 @@
+package com.example.store_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/store_v1/StoreV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.store_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
